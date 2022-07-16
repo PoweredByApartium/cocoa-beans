031fdc7315043d9d15de213670abcbd6f8b27d7b
Lior Slakman <me@voigon.dev>
2022-07-16 13:48:17 +0300

    Add Ensures

--- common/src/main/java/net/apartium/cocoabeans/Ensures.java
+++ common/src/main/java/net/apartium/cocoabeans/Ensures.java
@@ -10,5 +10,205 @@
 
 package net.apartium.cocoabeans;
 
+import java.util.Collection;
+import java.util.Map;
+
 public class Ensures {
+
+    /**
+     * Validates if a String is empty or not
+     *
+     * @param string the String instance to perform the validation on
+     * @param ex the exception to throw if the String is empty
+     * @throws RuntimeException ex if argument does not match requested precondition
+     */
+    public static void notEmpty(String string, RuntimeException ex) {
+        if (ex == null)
+            ex = new NullPointerException();
+
+        if (StringHelpers.isNullOrEmpty(string))
+            throw ex;
+    }
+
+    /**
+     * Checks if an object is null.
+     *
+     * @param obj the object instance to check
+     * @param ex  the exception to throw if the object is null, if null will be replaced by npe
+     * @throws RuntimeException ex if argument does not match requested precondition
+     */
+    public static void notNull(Object obj, RuntimeException ex) {
+        if (obj == null)
+            throw ex == null ? new NullPointerException() : ex;
+
+    }
+
+    /**
+     * Makes sure that given collection is not null or empty
+     *
+     * @param collection the collection to check
+     * @param ex the exception to throw if the object does not match requested precondition
+     * @throws RuntimeException ex if argument does not match requested precondition
+     */
+    public static void notEmpty(Collection<?> collection, RuntimeException ex) {
+        notNull(collection, ex);
+        if (collection.size() > 0)
+            throw ex;
+
+    }
+
+    /**
+     * Makes sure that given collection is not null or empty
+     *
+     * @param collection the collection to check
+     * @param message the message, "+-" will be replaced to "cannot be null or empty"
+     * @throws RuntimeException ex if argument does not match requested precondition
+     */
+    public static void notEmpty(Collection<?> collection, String message) {
+        notEmpty(collection, new NullPointerException(message.replaceAll("\\+-", "cannot be null or empty")));
+    }
+
+    /**
+     * Makes sure that given map is not null or empty
+     *
+     * @param map  the map to check
+     * @param ex the exception to throw if the object does not match requested precondition
+     * @throws RuntimeException ex if argument does not match requested precondition
+     */
+    public static void notEmpty(Map<?, ?> map, RuntimeException ex) {
+        notNull(ex, "ex +-");
+        notNull(map, ex);
+
+        if (map.size() > 0)
+            throw ex;
+
+    }
+
+    /**
+     * Makes sure that given map is not null or empty
+     *
+     * @param map the map object to check
+     * @param message the message, "+-" will be replaced to "cannot be null or empty"
+     * @throws RuntimeException if argument does not match requested precondition
+     */
+    public static void notEmpty(Map<?, ?> map, String message) {
+        notEmpty(map, new RuntimeException(message.replaceAll("\\+-", "cannot be null or empty")));
+    }
+
+    /**
+     * Make sure that given array is not null or empty
+     *
+     * @param array the array to check
+     * @param ex the exception to throw if the object is invalid
+     * @throws RuntimeException ex if argument does not match requested precondition
+     */
+    public static void notEmpty(Object[] array, RuntimeException ex) {
+        notNull(array, ex);
+
+        if (array.length > 0)
+            throw ex;
+
+    }
+
+    /**
+     * Makes sure that given array is not null or empty
+     *
+     * @param array the array to check
+     * @param message the message, "+-" will be replaced to "cannot be null or empty"
+     * @throws RuntimeException ex if argument does not match requested precondition
+     */
+    public static void notEmpty(Object[] array, String message) {
+        notEmpty(array, new RuntimeException(message.replaceAll("\\+-", "cannot be null or empty")));
+    }
+
+    /**
+     * Make sure an object is not null.
+     *
+     * @param obj the object to check
+     * @param message the message to throw with the exception if the object is null, +- will be replaced with "cannot be null"
+     * @throws NullPointerException ex if argument does not match requested precondition
+     */
+    public static void notNull(Object obj, String message) {
+        notNull(obj, new NullPointerException(message.replaceAll("\\+-", "cannot be null")));
+    }
+
+    /**
+     * Makes sure given string is not null or empty
+     *
+     * @param string the String instance to validate on
+     * @param message the message to throw with the exception if the String is empty, "+-" will be replaced to "cannot be null or empty"
+     * @throws RuntimeException if argument does not match requested precondition
+     */
+    public static void notEmpty(String string, String message) {
+        notEmpty(string, new RuntimeException(message.replaceAll("\\+-", "cannot be empty / null")));
+    }
+
+    /**
+     * Makes sure given number is larger than second given number.
+     *
+     * @param num1 the first number
+     * @param num2 the second number
+     * @param ex the exception to throw if not larger than
+     * @throws RuntimeException if argument does not match requested precondition
+     */
+    public static void largerThan(int num1, int num2, RuntimeException ex) {
+        if (num1 <= num2)
+            if (ex != null)
+                throw ex;
+    }
+
+    /**
+     * Makes sure given number is larger than second given number.
+     *
+     * @param num1 the first number
+     * @param num2 the second number
+     * @param message the message, "+-" will be replaced to "must be larger than"
+     * @throws RuntimeException if argument does not match requested precondition
+     */
+    public static void largerThan(int num1, int num2, String message) {
+        largerThan(num1, num2, new NullPointerException(message.replaceAll("\\+-", "must be larger than")));
+
+    }
+
+    /**
+     * Makes sure given condition is true
+     *
+     * @param bool the condition
+     * @throws RuntimeException if argument does not match requested precondition
+     */
+    public static void isTrue(boolean bool) {
+        isTrue(bool, new RuntimeException());
+
+    }
+
+    /**
+     * Makes sure given condition is true
+     *
+     * @param bool the condition
+     * @param message the message to send with the exception, "+-" will be replaced with "must be true"
+     * @throws RuntimeException if argument does not match requested precondition
+     */
+    public static void isTrue(boolean bool, String message) {
+        message = message.replaceAll("\\+-", "must be true");
+
+        isTrue(bool, new NullPointerException(message));
+
+    }
+
+    /**
+     *
+     * Makes sure given condition is true
+     *
+     * @param bool the condition
+     * @param ex the exception to throw if the boolean is false
+     * @throws RuntimeException ex if argument does not match requested precondition
+     */
+    public static void isTrue(boolean bool, RuntimeException ex) {
+        if (!bool)
+            throw ex == null ? new RuntimeException() : ex;
+
+
+    }
+
+
 }
