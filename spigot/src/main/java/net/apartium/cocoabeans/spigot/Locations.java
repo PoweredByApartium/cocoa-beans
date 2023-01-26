e90da6a88cd540ac458818a9c4c8f47574d1b3d9
Lior Slakman <me@voigon.dev>
2023-01-27 01:06:17 +0200

    Add missing documentation #6

--- spigot/src/main/java/net/apartium/cocoabeans/spigot/Locations.java
+++ spigot/src/main/java/net/apartium/cocoabeans/spigot/Locations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Apartium
+ * Copyright 2023 Apartium
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -17,6 +17,9 @@ import org.bukkit.World;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Bukkit locations systems helper methods
+ */
 public class Locations {
 
     /**
