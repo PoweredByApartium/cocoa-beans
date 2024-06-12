c4271c79bf56d928240c8c05ef5e74abfebce891
idan <idankob@gmail.com>
2024-06-12 21:36:14 +0300

    Initial commit

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -6,6 +6,8 @@ group = "net.apartium.cocoa-beans"
 version = parent!!.version
 
 dependencies {
+    testImplementation("commons-codec:commons-codec:1.17.0")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:${project.findProperty("junit.version")}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${project.findProperty("junit.version")}")
 }
