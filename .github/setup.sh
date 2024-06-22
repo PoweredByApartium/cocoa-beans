4f8c192eec4cb0e0ee3b118401a6c8539d8e3809
idan <idankob@gmail.com>
2024-06-22 12:46:01 +0300

    Should work

--- .github/setup.sh
+++ .github/setup.sh
@@ -4,13 +4,10 @@ java -version # Debug only
 
 ./gradlew javadoc
 
-mkdir docs
 mkdir -p docs/common
 mkdir -p docs/spigot
 mkdir -p docs/commands-spigot
 mkdir -p docs/commands
-ls -la artifacts
-unzip artifacts/webHelpCB2-all.zip -d docs
 ls docs/
 cp -a common/build/docs/javadoc/* docs/common
 cp -a spigot/build/docs/javadoc/* docs/spigot
