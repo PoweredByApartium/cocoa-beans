d893b9f3277064a96cffaaa4f1bdb932b5691ab0
idan <idankob@gmail.com>
2024-07-01 13:20:24 +0300

    .

--- build-scripts/prepare-web-output.sh
+++ build-scripts/prepare-web-output.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-mkdir docs
+mkdir $docs
 unzip artifacts/webHelpCB2-all.zip -d docs
-mkdir -p docs/common
-mkdir -p docs/spigot
-mkdir -p docs/commands-spigot
-mkdir -p docs/commands
-ls docs/
-cp -a common/build/docs/javadoc/* docs/common
-cp -a spigot/build/docs/javadoc/* docs/spigot
-cp -a commands/build/docs/javadoc/* docs/commands
-cp -a commands/spigot-platform/build/docs/javadoc/* docs/commands-spigot
+mkdir -p $docs/common
+mkdir -p $docs/spigot
+mkdir -p $docs/commands-spigot
+mkdir -p $docs/commands
+
+ls $docs/
+cp -a common/build/docs/javadoc/* $docs/common
+cp -a spigot/build/docs/javadoc/* $docs/spigot
+cp -a commands/build/docs/javadoc/* $docs/commands
+cp -a commands/spigot-platform/build/docs/javadoc/* $docs/commands-spigot
