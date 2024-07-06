a7c8e9719eb4c86847e54bff28c99cb9db523792
idan <idankob@gmail.com>
2024-07-06 23:09:42 +0300

    Temp commit

--- build-scripts/prepare-web-output.sh
+++ build-scripts/prepare-web-output.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-mkdir $docs
-unzip artifacts/webHelpCB2-all.zip -d $docs
-mkdir -p $docs/common
-mkdir -p $docs/spigot
-mkdir -p $docs/commands-spigot
-mkdir -p $docs/commands
+if [ ! -d "docs" ]; then
+  mkdir $docs
+  unzip artifacts/webHelpCB2-all.zip -d $docs
+  mkdir -p $docs/common
+  mkdir -p $docs/spigot
+  mkdir -p $docs/commands-spigot
+  mkdir -p $docs/commands
 
-ls $docs/
-cp -a common/build/docs/javadoc/* $docs/common
-cp -a spigot/build/docs/javadoc/* $docs/spigot
-cp -a commands/build/docs/javadoc/* $docs/commands
-cp -a commands/spigot-platform/build/docs/javadoc/* $docs/commands-spigot
+  ls $docs/
+  cp -a common/build/docs/javadoc/* $docs/common
+  cp -a spigot/build/docs/javadoc/* $docs/spigot
+  cp -a commands/build/docs/javadoc/* $docs/commands
+  cp -a commands/spigot-platform/build/docs/javadoc/* $docs/commands-spigot
+fi
