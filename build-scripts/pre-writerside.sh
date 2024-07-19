ba6dfb76b3606881cdd803b0d9bea76713a2924e
idan <idankob@gmail.com>
2024-07-19 16:36:01 +0300

    Should fix plugin jar file being pushed to main

--- build-scripts/pre-writerside.sh
+++ build-scripts/pre-writerside.sh
@@ -6,6 +6,7 @@ writerside="Writerside/writerside.cfg"
 sed -i 's/\(<instance.*version="\)[^"]*"/\1'"$new_version"'"/' "$writerside"
 sed -i "s/\(<var name=\"version\" value=\"\)[^\"]*\(\"\/>\)/\1$new_version\2/" "$vars"
 
-git add .
+git add Writerside/writerside.cfg
+git add Writerside/v.list
 git commit -m "Update writerside.cfg and home.md" || git diff --staged --quiet
 git push origin $branch || echo "No changes to push"
