53999a9fcd475cddd72162267e53e7fd2bde431d
idan <idankob@gmail.com>
2024-07-23 21:13:04 +0300

    .

--- build-scripts/pre-writerside.sh
+++ build-scripts/pre-writerside.sh
@@ -5,6 +5,3 @@ writerside="Writerside/writerside.cfg"
 
 sed -i 's/\(<instance.*version="\)[^"]*"/\1'"$new_version"'"/' "$writerside"
 sed -i "s/\(<var name=\"version\" value=\"\)[^\"]*\(\"\/>\)/\1$new_version\2/" "$vars"
-
-git add Writerside/v.list
-git add Writerside/writerside.cfg
