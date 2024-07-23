7bf04e003c1a630ba424f7594a4fbe554f86c0c4
Idan <idankob@gmail.com>
2024-07-23 23:44:33 +0300

    [cicd] Release action side effect fix

--- build-scripts/pre-writerside.sh
+++ build-scripts/pre-writerside.sh
@@ -5,7 +5,3 @@ writerside="Writerside/writerside.cfg"
 
 sed -i 's/\(<instance.*version="\)[^"]*"/\1'"$new_version"'"/' "$writerside"
 sed -i "s/\(<var name=\"version\" value=\"\)[^\"]*\(\"\/>\)/\1$new_version\2/" "$vars"
-
-git add .
-git commit -m "Update writerside.cfg and home.md" || git diff --staged --quiet
-git push origin $branch || echo "No changes to push"
