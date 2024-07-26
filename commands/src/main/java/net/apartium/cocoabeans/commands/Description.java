c7446b0ca14363f611461b3d5ffbca13d64cd406
CyberRei <143542228+CyberRei@users.noreply.github.com>
2024-07-26 20:00:32 +0300

    Added all 3 annotations

---
+++ commands/src/main/java/net/apartium/cocoabeans/commands/Description.java
@@ -0,0 +1,17 @@
+package net.apartium.cocoabeans.commands;
+
+import org.jetbrains.annotations.ApiStatus;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiStatus.AvailableSince("0.0.30")
+public @interface Description {
+
+    String value();
+
+}
