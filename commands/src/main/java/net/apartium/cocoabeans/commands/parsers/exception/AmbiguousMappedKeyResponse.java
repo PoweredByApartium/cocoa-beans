55a5ecebe62623e4221709302dc8d256c0882557
Lior S <me@voigon.dev>
2024-08-23 17:53:49 +0300

    [docs] Fix jd issue

--- commands/src/main/java/net/apartium/cocoabeans/commands/parsers/exception/AmbiguousMappedKeyResponse.java
+++ commands/src/main/java/net/apartium/cocoabeans/commands/parsers/exception/AmbiguousMappedKeyResponse.java
@@ -39,7 +39,7 @@ public class AmbiguousMappedKeyResponse extends InvalidParserResponse {
     }
 
     /**
-     * @inheritDoc
+     * {@inheritDoc}
      */
     @Override
     public AmbiguousMappedKeyException getError() {
