9116f011ff4d028ff31c1a649153d54831ab323a
Lior S <me@voigon.dev>
2024-10-18 01:01:06 +0300

    [cicd] Add SonarQube

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,13 +68,15 @@ allprojects {
         }
     }
 
+}
+
+subprojects {
     sonar {
         properties {
             property("sonar.projectKey", "PoweredByApartium_cocoa-beans_323780ff-4d56-4f0c-8ad6-d383a7c42a80")
             property("sonar.projectName", "cocoa-beans")
         }
     }
-
 }
 
 hangarPublish {
