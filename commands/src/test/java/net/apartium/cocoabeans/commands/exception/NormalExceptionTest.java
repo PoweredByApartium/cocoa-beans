cfa2fcadcc8fba50d273a290ee491f0cde842bce
kfir <kfirbotnik@gmail.com>
2024-12-30 14:58:15 +0200

    Fixes sonar issue

--- commands/src/test/java/net/apartium/cocoabeans/commands/exception/NormalExceptionTest.java
+++ commands/src/test/java/net/apartium/cocoabeans/commands/exception/NormalExceptionTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class NormalExceptionTest extends CommandTestBase {
+class NormalExceptionTest extends CommandTestBase {
 
     @BeforeEach
     @Override
