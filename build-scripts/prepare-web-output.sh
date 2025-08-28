db0f2fe4f4a3b3359cc838dcc54106e13a3280e1
idank <idankob@gmail.com>
2025-08-28 18:57:01 +0300

    Should fix writerside cicd failure

--- build-scripts/prepare-web-output.sh
+++ build-scripts/prepare-web-output.sh
@@ -18,11 +18,11 @@ if [ ! -d "docs" ]; then
   cp -a cocoa-beans-common/build/docs/javadoc/* $docs/common
   cp -a cocoa-beans-state/build/docs/javadoc/* $docs/state
   cp -a cocoa-beans-minestom/build/docs/javadoc/* $docs/minestom
-  cp -a cocoa-beans-state/spigot/build/docs/javadoc/* $docs/state-spigot
+  cp -a cocoa-beans-state-spigot/build/docs/javadoc/* $docs/state-spigot
   cp -a cocoa-beans-spigot/build/docs/javadoc/* $docs/spigot
   cp -a cocoa-beans-commands/build/docs/javadoc/* $docs/commands
-  cp -a cocoa-beans-commands/spigot/build/docs/javadoc/* $docs/commands-spigot
+  cp -a cocoa-beans-commands-spigot/build/docs/javadoc/* $docs/commands-spigot
   cp -a cocoa-beans-scoreboard/build/docs/javadoc/* $docs/scoreboard
   cp -a cocoa-beans-scoreboard-spigot/build/docs/javadoc/* $docs/scoreboard-spigot
-  cp -a cocoa-beans-scoreboard/minestom/build/docs/javadoc/* $docs/scoreboard-minestom
+  cp -a cocoa-beans-scoreboard-minestom/build/docs/javadoc/* $docs/scoreboard-minestom
 fi
