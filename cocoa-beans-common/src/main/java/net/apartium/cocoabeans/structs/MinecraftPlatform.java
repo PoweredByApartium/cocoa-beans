36cb67477e77e65410a24768ef6468d7dfbddaa8
Lior S. <me@voigon.dev>
2026-02-10 18:56:57 +0200

    Add more docs

--- cocoa-beans-common/src/main/java/net/apartium/cocoabeans/structs/MinecraftPlatform.java
+++ cocoa-beans-common/src/main/java/net/apartium/cocoabeans/structs/MinecraftPlatform.java
@@ -2,6 +2,16 @@ package net.apartium.cocoabeans.structs;
 
 import org.jetbrains.annotations.ApiStatus;
 
+/**
+ * Represents a Minecraft platform, including its version, platform name, and platform version.
+ * <p>
+ * This record is used to encapsulate information about a specific Minecraft platform instance.
+ * </p>
+ * @param version the Minecraft version
+ * @param platformName the name of the platform (e.g., Spigot, Minestom)
+ * @param platformVersion the version of the platform
+ * @since 0.0.46
+ */
 @ApiStatus.AvailableSince("0.0.46")
 public record MinecraftPlatform(
         MinecraftVersion version,
--- cocoa-beans-common/src/main/java/net/apartium/cocoabeans/structs/NamespacedKey.java
+++ cocoa-beans-common/src/main/java/net/apartium/cocoabeans/structs/NamespacedKey.java
@@ -2,6 +2,16 @@ package net.apartium.cocoabeans.structs;
 
 import org.jetbrains.annotations.ApiStatus;
 
+/**
+ * Represents a namespaced key, consisting of a namespace and a key string.
+ * <p>
+ * Used to uniquely identify resources or objects within a specific namespace, avoiding naming collisions.
+ * Commonly used in Minecraft and similar systems for resource identification.
+ * </p>
+ * @param namespace the namespace part of the key (e.g., "minecraft", "apartium")
+ * @param key the key part of the namespaced key (e.g., "stone", "custom_item")
+ * @since 0.0.46
+ */
 @ApiStatus.AvailableSince("0.0.46")
 public record NamespacedKey(String namespace, String key) {
 }
